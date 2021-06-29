594dae0e6f8da5f02048656d4b11eb8df11bdc9b
andremartds <andremartinsdsilva@gmail.com>
2021-06-29 00:36:16 -0300

    adicionando notEmpty em dto

--- src/main/java/andremartds/library/api/dto/BookDTO.java
+++ src/main/java/andremartds/library/api/dto/BookDTO.java
@@ -1,8 +1,18 @@
 package andremartds.library.api.dto;
 import lombok.*;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class BookDTO { private Long id; private String title; private String autor; private String isbn; }
+public class BookDTO {
+    private Long id;
+    @NotEmpty(message = "title não pode ser nulo")
+    private String title;
+    @NotEmpty(message = "autor não pode ser nulo")
+    private String autor;
+    @NotEmpty(message = "isbn não pode ser nulo")
+    private String isbn;
+}
