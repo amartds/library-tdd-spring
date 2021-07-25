cefaa82ac280996a8db90180b614a5d008c06a88
andremartds <andremartinsdsilva@gmail.com>
2021-07-25 12:41:23 -0300

    testando caso de erro em service

--- src/test/java/andremartds/library/service/BookServiceTest.java
+++ src/test/java/andremartds/library/service/BookServiceTest.java
@@ -1,8 +1,11 @@
 package andremartds.library.service;
 
+import andremartds.library.api.dto.BookDTO;
+import andremartds.library.api.exceptions.BusinessException;
 import andremartds.library.model.entity.Book;
 import andremartds.library.model.entity.repository.BookRepository;
 import andremartds.library.service.impl.BookServiceImpl;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -29,7 +32,8 @@ public class BookServiceTest {
    @DisplayName("deve salvar um livro")
    public void saveBookTest() {
        // expected
-       Book book = Book.builder().isbn("123").autor("Fulano").title("aventuras").build();
+       Book book = this.createNewBook();
+       Mockito.when(respository.existsByIsbn(Mockito.anyString())).thenReturn(false);
 
        Mockito.when(respository.save(book))
                .thenReturn(Book.builder().id(1L).isbn("123").autor("Fulano").title("aventuras").build());
@@ -43,4 +47,24 @@ public class BookServiceTest {
        assertThat(savedBook.getAutor()).isEqualTo("Fulano");
        assertThat(savedBook.getTitle()).isEqualTo("aventuras");
    }
+
+    @Test
+    @DisplayName("Deve lancar erro de negocio ao tentar salvar um livro com isbn duplicado")
+    public void shouldNotSaveBookWithIsbnDuplicated() {
+       // cenario
+       Book book = createNewBook();
+       Mockito.when(respository.existsByIsbn(Mockito.anyString())).thenReturn(true);
+
+       //execucao
+       Throwable ex = Assertions.catchThrowable(() -> service.save(book));
+       assertThat(ex)
+               .isInstanceOf(BusinessException.class)
+               .hasMessage("isbn já cadastrado.");
+
+       Mockito.verify(respository, Mockito.never()).save(book);
+   }
+
+    private Book createNewBook() {
+        return Book.builder().autor("André Martins").title("Meu livro").isbn("123123").build();
+    }
 }
